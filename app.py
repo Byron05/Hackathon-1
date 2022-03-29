f30ffad7f7cc13697ef22c07b41f85ebbd719137
nicolekwon <56002536+nicolekwon@users.noreply.github.com>
2022-03-29 09:56:50 -0400

    Initialized Flask app with SocketIO

---
+++ app.py
@@ -0,0 +1,63 @@
+# save this as app.py
+from flask import Flask, escape, request, json, render_template, request, redirect, url_for
+from flask_socketio import SocketIO, join_room, leave_room
+
+import getpass
+import pymongo
+
+from random import randint
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return "<p>Hello, World!</p>"
+
+
+@app.route('/chathome')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
+    else:
+        return redirect(url_for('home'))
+
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+    app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
+                                                                    data['room'],
+                                                                    data['message']))
+    socketio.emit('receive_message', data, room=data['room'])
+
+
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data, room=data['room'])
+
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
+    leave_room(data['room'])
+    socketio.emit('leave_room_announcement', data, room=data['room'])
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
